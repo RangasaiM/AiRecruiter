5e48db0ae09b4d0dea2862085ece4b9d8142a1e1
Rangasai <rangasaimangalagiri@gmail.com>
2025-05-29 19:29:10 +0530

    implemented user dashboard

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
+import DashboardLayout from "./(main)/layout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
