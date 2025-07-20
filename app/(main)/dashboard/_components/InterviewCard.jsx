166d14541e266936113151d3fdff9f9786676a2d
Rangasai <rangasaimangalagiri@gmail.com>
2025-07-20 21:40:43 +0530

    Enhance InterviewCard component with detail view and candidate count
    This update modifies the InterviewCard component to include an optional detail view and displays the number of candidates associated with the interview. It also adds an ArrowRight icon for the detail button and improves the layout for better readability.

--- app/(main)/dashboard/_components/InterviewCard.jsx
+++ app/(main)/dashboard/_components/InterviewCard.jsx
@@ -1,38 +1,40 @@
 import { Button } from '@/components/ui/button'
-import { Copy, Send } from 'lucide-react'
+import { ArrowRight, Copy, Send } from 'lucide-react'
 import moment from 'moment'
 import React from 'react'
 import { toast } from 'sonner'
 
-function InterviewCard({interview}) {
+function InterviewCard({ interview, viewDetail = false }) {
 
 
-    const copyLink = ()=>{
-        const url=process.env.NEXT_PUBLIC_HOST_URL+'/'+interview?.interview_id
-        navigator.clipboard.writeText(url);
-        toast.success('Link copied to clipboard');
-      }
+  const copyLink = () => {
+    const url = process.env.NEXT_PUBLIC_HOST_URL + '/' + interview?.interview_id
+    navigator.clipboard.writeText(url);
+    toast.success('Link copied to clipboard');
+  }
 
-      const onSend=()=>{
-        toast.loading('Sending email...');
-        window.location.href='mailto:rangasaimangalagiri@gmail.com?subject=Interview+Link&body=Check+out+this+interview+link:+'+process.env.NEXT_PUBLIC_HOST_URL+'/'+interview?.interview_id;
-        toast.success('Email sent successfully');
-      }
+  const onSend = () => {
+    toast.loading('Sending email...');
+    window.location.href = 'mailto:rangasaimangalagiri@gmail.com?subject=Interview+Link&body=Check+out+this+interview+link:+' + process.env.NEXT_PUBLIC_HOST_URL + '/' + interview?.interview_id;
+    toast.success('Email sent successfully');
+  }
   return (
     <div className='p-5 bg-white rounded-lg border'>
-        <div className='flex items-center justify-between'>
-            <div className='h-[40px] w-[40px] rounded-full bg-primary'>
-            </div>
-            <h2 className='text-sm'>{moment(interview?.created_at).format('DD MMM yyy')}</h2>
-        
+      <div className='flex items-center justify-between'>
+        <div className='h-[40px] w-[40px] rounded-full bg-primary'>
         </div>
-        
-        <h2 className='mt-3 font-bold text-lg'>{interview?.jobPosition}</h2>
-        <h2 className='mt-2'>{interview?.duration}</h2>
-        <div className='flex gap-3 w-full mt-5'>
-            <Button variant={'outline'} className='text-sm' onClick={()=>copyLink()}><Copy /> Copy Link</Button>
-            <Button className='text-sm' onClick={()=>onSend()}><Send /> Send</Button>
-        </div>
+        <h2 className='text-sm'>{moment(interview?.created_at).format('DD MMM yyy')}</h2>
+
+      </div>
+
+      <h2 className='mt-3 font-bold text-lg'>{interview?.jobPosition}</h2>
+      <h2 className='mt-2 flex justify-between text-gray-500'>{interview?.duration}  
+        <span className='text-green-700'>{interview['interview-feedback']?.length} Candidates</span>
+      </h2>
+      {!viewDetail ? <div className='flex gap-3 w-full mt-5'>
+        <Button variant={'outline'} className='text-sm' onClick={() => copyLink()}><Copy /> Copy Link</Button>
+        <Button className='text-sm' onClick={() => onSend()}><Send /> Send</Button>
+      </div> : <Button className='mt-5 w-full' variant='outline'>View Detail <ArrowRight /> </Button>}
     </div>
   )
 }
