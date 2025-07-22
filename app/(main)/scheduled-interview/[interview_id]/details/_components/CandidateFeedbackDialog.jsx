8182b36d2021135a805ba975f7830266221a85c3
Rangasai <rangasaimangalagiri@gmail.com>
2025-07-22 12:53:31 +0530

    Enhance CandidateFeedbackDialog with performance summary and recommendation display
    This update adds a new section for performance summary and improves the recommendation display within the CandidateFeedbackDialog component. The layout has been adjusted for better readability and user experience.

--- app/(main)/scheduled-interview/[interview_id]/details/_components/CandidateFeedbackDialog.jsx
+++ app/(main)/scheduled-interview/[interview_id]/details/_components/CandidateFeedbackDialog.jsx
@@ -37,26 +37,43 @@ function CandidateFeedbackDialog({ candidate }) {
                                     <h2 className="text-primary text-2xl font-bold">6/10</h2>
                                 </div>
                             </div>
-                            <div>
+                            <div className='mt-5'>
                                 <h2 className='font-bold'>Skills Assesment</h2>
                                 <div className='mt-3 grid grid-cols-2 gap-10'>
                                     <div>
                                         <h2 className='flex justify-between'>Technical Skills <span>{feedback?.feedback?.rating?.techicalSkills}/10</span></h2>
-                                        <Progress value={feedback?.feedback?.rating?.techicalSkills*10} className='mt-1' />
+                                        <Progress value={feedback?.feedback?.rating?.techicalSkills * 10} className='mt-1' />
                                     </div>
                                     <div>
                                         <h2 className='flex justify-between'>Communication <span>{feedback?.feedback?.rating?.communication}/10</span></h2>
-                                        <Progress value={feedback?.feedback?.rating?.communication*10} className='mt-1' />
+                                        <Progress value={feedback?.feedback?.rating?.communication * 10} className='mt-1' />
                                     </div>
                                     <div>
                                         <h2 className='flex justify-between'>Problem Solving <span>{feedback?.feedback?.rating?.problemSolving}/10</span></h2>
-                                        <Progress value={feedback?.feedback?.rating?.problemSolving*10} className='mt-1' />
+                                        <Progress value={feedback?.feedback?.rating?.problemSolving * 10} className='mt-1' />
                                     </div>
                                     <div>
                                         <h2 className='flex justify-between'>Experience <span>{feedback?.feedback?.rating?.experince}/10</span></h2>
-                                        <Progress value={feedback?.feedback?.rating?.experince*10} className='mt-1' />
+                                        <Progress value={feedback?.feedback?.rating?.experince * 10} className='mt-1' />
                                     </div>
                                 </div>
+                            </div>
+                            <div className='mt-5'>
+                                <h2 className='font-bold'>Performance Summery</h2>
+                                <div className='p-5 bg-secondary my-3 rounded-md'>
+                                    {feedback?.feedback?.summary?.map((summery, index) => (
+                                        <p key={index}>{summery}</p>
+                                    ))}
+                                </div>
+                            </div>
+
+                            <div className={`p-5 mt-10 flex items-center justify-between rounded-md ${feedback?.feedback?.
+                                recommendation == 'No' ? 'bg-red-100' : 'bg-green-100'}`}>
+                                <div>
+                                    <h2 className={`font-bold ${feedback?.feedback?.recommendation == 'No' ? 'text-red-700' : 'text-green-700'}`}>Recommendation Msg: {feedback?.feedback?.recommendation}</h2>
+                                    <p className={`${feedback?.feedback?.recommendation == 'No' ? 'text-red-500' : 'text-green-500'}`}>{feedback?.feedback?.recommendationMsg}</p>
+                                </div>
+                                <Button className={`${feedback?.feedback?.recommendation == 'No' ? 'bg-red-700' : 'bg-green-700'}`}>Send Msg</Button>
                             </div>
                         </div>
                     </DialogDescription>
